639787846d29fadaf3ce59fff8953335b2e327b9
Arbbz <arbaashawla@gmail.com>
2025-08-26 00:11:30 +0700

    Update Page:
    - Added 'dashboard' landing page.
    - Created header component for non-auth pages.
    - Fixed 'app.blade.php' to handle non-auth pages.
    - Implemented 'payment' page for viewing offers.
    - Created dummy premium question redirecting to payment page.
    - Added offer display during registration process."

--- resources/views/components/layouts/micro/footer.blade.php
+++ resources/views/components/layouts/micro/footer.blade.php
@@ -1,20 +1,45 @@
-
 <!-- Knowing is not enough; we must apply. Being willing is not enough; we must do. - Leonardo da Vinci -->
 <footer class="pc-footer">
-    <div class="footer-wrapper container-fluid">
+    <div class="footer-wrapper container-fluid ">
         <div class="row">
             <div class="col my-1">
-                <p class="m-0"
-                >Academic Stride &#9829; crafted by Ma Chung University</p
-                >
+                <p class="m-0">
+                    Academic Stride &#9829; crafted by Ma Chung University
+                </p>
             </div>
             <div class="col-auto my-1">
                 <ul class="list-inline footer-link mb-0">
-                    @if(auth()->user()->hasAnyRole('Educator','Analyser'))
-                        <li class="list-inline-item"><a href="{{ route('homes') }}">Home</a></li>
-                    @elseif(auth()->user()->hasRole('Student'))
-                        <li class="list-inline-item"><a href="{{ route('active-exam') }}">Home</a></li>
-                    @endif
+                    @auth
+                        @if(auth()->user()->hasAnyRole('Educator','Analyser'))
+                            <li class="list-inline-item">
+                                <a href="{{ route('homes') }}">Home</a>
+                            </li>
+                        @elseif(auth()->user()->hasRole('Student'))
+                            <li class="list-inline-item">
+                                <a href="{{ route('active-exam') }}">Home</a>
+                            </li>
+                        @endif
+                        <li class="list-inline-item">
+                            <a href="#">Privacy Policy</a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="#">Support</a>
+                        </li>
+                    @else
+                        {{-- Footer links for non-authenticated users --}}
+                        <li class="list-inline-item">
+                            <a href="{{ url('/') }}">Home</a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="#">About</a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="#">Contact</a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="#">Privacy Policy</a>
+                        </li>
+                    @endauth
                 </ul>
             </div>
         </div>
---
+++ resources/views/components/layouts/micro/guest-header.blade.php
@@ -0,0 +1,12 @@
+<header class="navbar navbar-expand-lg bg-light p-3">
+    <div class="container">
+        <a class="navbar-brand" href="#">
+            <img src="/logo/Logo.png" alt="Garuda Cerdas Logo" height="30">
+        </a>
+
+        <div class="ms-auto d-flex align-items-center">
+            <a href="#" class="nav-link text-decoration-none me-3 text-dark">Login</a>
+            <a href="#" class="btn btn-danger">Daftar</a>
+        </div>
+    </div>
+</header>
